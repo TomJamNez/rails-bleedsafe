9d83457bc6ea6cc7929a029aaff0ea2f464bf54f
Samuel Long <samuell0ng2024@gmail.com>
2025-05-27 15:02:34 +0100

    First Aid Map

---
+++ app/models/first_aid_map.rb
@@ -0,0 +1,2 @@
+class FirstAidMap < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_27_113946) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_27_135939) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "first_aid_maps", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.decimal "longitude"
+    t.decimal "latitude"
+    t.string "category"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "training_modules", force: :cascade do |t|
     t.string "name"
