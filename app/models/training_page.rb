e0cb179d92e69d921e354336671faa3020611f42
Abiola Oladunni <abiolaola85@gmail.com>
2025-05-28 14:07:42 +0100

    Training model validations and associations completed

--- app/models/training_page.rb
+++ app/models/training_page.rb
@@ -1,3 +1,5 @@
 class TrainingPage < ApplicationRecord
   belongs_to :training_topic
+  has_one_attached :photo
+  validates :name, presence: true
 end
--- app/models/training_topic.rb
+++ app/models/training_topic.rb
@@ -1,3 +1,6 @@
 class TrainingTopic < ApplicationRecord
   belongs_to :training_module
+  has_many :training_pages
+  has_one_attached :photo
+  validates :name, presence: true
 end
