2d6ac1a0024b0b95e4b843de0540e5785c27687c
Thomas Jamshidnezhad thomas.jamshidnezhad@gmail.com <thomas.jamshidnezhad@gmail.com>
2025-05-28 12:01:49 +0100

    push part complete change

--- app/controllers/emergency_modules_controller.rb
+++ app/controllers/emergency_modules_controller.rb
@@ -1,13 +1,48 @@
 class EmergencyModulesController < ApplicationController
   def index
+    @emergency_modules = EmergencyModule.all
+
   end
 
   def show
+    @emergency_module = EmergencyModule.find[params[:id]]
+    @emergency_topics = @emergency_module.emergency_topics.where(active: true)
   end
 
   def new
+    @emergency_module = EmergencyModule.new()
+  end
+
+  def create
+    @emergency_module = EmergencyModule.create(module_params)
+    #needs updating after pundit, should be false by default, making true now for testing and demo.
+    @emergency_module.active = true
+    if @emergency_module.save
+      #sort redirect back to object instead of index, leaving like this as easier for people to learn what the app is doing. This is admin only so not important
+      redirect_to emergency_modules_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @emergency_module = EmergencyModule.find(params[:id])
   end
+
+  def update
+    @emergency_module = EmergencyModule.find(params[:id])
+    if @emergency_module.update(module_params)
+      redirect_to emergency_module_path(emergency_module)
+      else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def module_params
+    #needs updating after active storage
+    params.requier(:emergency_module).permit(:name, :active)
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_27_135939) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_28_100313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_27_135939) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "active"
     t.index ["user_id"], name: "index_emergency_modules_on_user_id"
   end
 
