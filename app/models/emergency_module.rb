3e8cf1d2fd5b309fbae099e75ada18afdeb059d9
Thomas Jamshidnezhad thomas.jamshidnezhad@gmail.com <thomas.jamshidnezhad@gmail.com>
2025-05-28 13:07:33 +0100

    completed rails-bleedsafe task 25

--- app/models/emergency_module.rb
+++ app/models/emergency_module.rb
@@ -2,6 +2,7 @@ class EmergencyModule < ApplicationRecord
   belongs_to :user
   has_many :emergency_topics
   has_many :emergency_pages, through: :emergency_topics
+  has_one_attached :photo
 
   validates :name, presence: true
 
